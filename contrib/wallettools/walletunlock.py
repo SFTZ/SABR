cd27fd1890e756683cb2fd7c1506e5f310988552
Laser9un <laser9un@gmail.com>
2018-05-28 12:48:18 +0300

    fix wallettools .py SABR RPC Port

--- contrib/wallettools/walletunlock.py
+++ contrib/wallettools/walletunlock.py
@@ -1,5 +1,5 @@
 from jsonrpc import ServiceProxy
-access = ServiceProxy("http://127.0.0.1:1331")
+access = ServiceProxy("http://127.0.0.1:2662")
 pwd = raw_input("Enter wallet passphrase: ")
 access.walletpassphrase(pwd, 60)
 
